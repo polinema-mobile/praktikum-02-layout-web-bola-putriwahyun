edf6461b7c2335090c434a3916a6b96c7efbee37
= <=>
2021-02-16 11:30:06 +0700

    Material Design dan iOS Cupertino

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 void main() {
   runApp(MyApp());
@@ -8,8 +9,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
     Widget build(BuildContext context) {
-    return MaterialApp(
-    home:Image(image: NetworkImage('https://hawaiibirdingtrails.hawaii.gov/wp-content/uploads/Barn-Owl_Sherman-Wing-1024x1024.jpg'),),
-    );
-  }
+      return MaterialApp(
+      home: Container(
+        margin: EdgeInsets.only(top: 30),
+        color: Colors.white,
+        child: Column(
+          children: <Widget>[
+            AppBar(title: Text('Contoh Cupertino')),
+            CupertinoButton(
+              child: Text("Contoh button"),
+              onPressed: () {},
+            ),
+            CupertinoActivityIndicator(),],
+          ),
+        ),
+      );
+    }
 }
