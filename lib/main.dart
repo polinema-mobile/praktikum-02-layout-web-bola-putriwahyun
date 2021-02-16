42e3980de7d98015a4c5430403b1bc5250b6b799
= <=>
2021-02-16 21:24:13 +0700

    Property Decoration

--- lib/main.dart
+++ lib/main.dart
@@ -9,24 +9,23 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Transform")),
+          appBar: AppBar(title: Text("Contoh Configuration")),
           body: Container(
-            decoration: BoxDecoration(
-              image: const DecorationImage(
-                image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg'),
-              fit: BoxFit.fitWidth,
-            ),
-            border: Border.all(
-              color: Colors.black,
-              width: 8,
-            ),
-          ),
-          height: 200,
-          width: 300,
-          margin: const EdgeInsets.only(left: 30.0, right: 30.0, top: 30),
-          transform: Matrix4.rotationZ(-0.1),
-          )
-      ),
+              decoration: BoxDecoration(
+                color: const Color(0xff7c94b6),
+                image: const DecorationImage(
+                  image: NetworkImage('https://upload.wikimedia.org/wikipedia/commons/8/8d/American_bison_k5680-1.jpg'),
+                  fit: BoxFit.fitWidth,
+                ),
+                border: Border.all(
+                  color: Colors.black,
+                  width: 8,
+                ),
+                borderRadius: BorderRadius.circular(12),
+              ),
+              height: 200,
+              width: 300,
+              margin: EdgeInsets.all(20))),
     );
   }
 }
