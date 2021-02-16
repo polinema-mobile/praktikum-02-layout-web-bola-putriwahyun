c444247adf39314b247571b584edfd3aea6f92cc
= <=>
2021-02-16 19:39:33 +0700

    Property Height and Width

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Color")),
-          body: Container(
-              margin: EdgeInsets.all(50),
-              height: 200,
-              width: 200,
-              alignment: Alignment.center,
-              color: Colors.amber[900],
-              child: Text(
-                'Semangat Belajar',
-                style: TextStyle(fontSize: 20, color: Colors.white),
-              ))),
-    );
+        appBar: AppBar(title: Text("Contoh Height dan Width")),
+        body: Container(
+            margin: EdgeInsets.all(50),
+            height: 200,
+            width: 200,
+            alignment: Alignment.center,
+            color: Colors.blueGrey,
+            child: Text(
+              'Semangat Belajar',
+              style: TextStyle(fontSize: 20, color: Colors.white),
+            ))),
+        );
   }
 }
