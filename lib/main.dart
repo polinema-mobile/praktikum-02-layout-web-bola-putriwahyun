0d795d41f7187facb4e49222c9d9983f28224c8a
= <=>
2021-02-16 19:28:08 +0700

    Contoh Alignment

--- lib/main.dart
+++ lib/main.dart
@@ -9,17 +9,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(title: Text("Contoh Button")),
-          body: Container(
-              child: RaisedButton(
-                textColor: Colors.white,
-                onPressed: (){},
-                color: Colors.red[900],
-                child: Text(
-                        "Button",
-                        style: TextStyle(fontSize: 20)
-                      ),
-              ))),
+        appBar: AppBar(title: Text("Contoh Aligment")),
+        body: Container(
+          alignment: Alignment.bottomCenter,
+          child: Text(
+            'Semangat Belajar',
+            style: TextStyle(
+              fontSize: 20,
+            ),
+          ))),
     );
   }
 }
